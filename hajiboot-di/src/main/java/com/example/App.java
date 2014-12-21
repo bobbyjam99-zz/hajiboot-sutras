b2b2a0f38001447be412e2c2c6f2a781e8b4e5ed
Tomokazu IMAI <bobbyjam99@gmail.com>
2014-12-21 14:27:05 +0900

    2.1.3 オート・ワイヤリングによるDI

--- hajiboot-di/src/main/java/com/example/App.java
+++ hajiboot-di/src/main/java/com/example/App.java
@@ -1,8 +1,6 @@
 package com.example;
 
-import com.example.app.Argument;
-import com.example.app.ArgumentResolver;
-import com.example.app.Calculator;
+import com.example.app.Frontend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.ConfigurableApplicationContext;
@@ -14,13 +12,8 @@ public class App {
     public static void main(String[] args) {
 
         try (ConfigurableApplicationContext context = SpringApplication.run(App.class, args)) {
-            System.out.println("Enter 2 numbers like 'a b' :");
-            ArgumentResolver argumentResolver = context.getBean(ArgumentResolver.class);
-            Argument argument = argumentResolver.resolve(System.in);
-            Calculator calculator = context.getBean(Calculator.class);
-            int result = calculator.calc(argument.getA(), argument.getB());
-
-            System.out.println("result = " + result);
+            Frontend frontend = context.getBean(Frontend.class);
+            frontend.run();
         }
     }
 }
