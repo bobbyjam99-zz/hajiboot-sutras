c6319b8959eee4e7d900303cf32cd0f623d37817
Tomokazu IMAI <bobbyjam99@gmail.com>
2014-12-21 14:34:45 +0900

    2.1.5 CommandLineRunner の利用

--- hajiboot-di/src/main/java/com/example/App.java
+++ hajiboot-di/src/main/java/com/example/App.java
@@ -1,19 +1,34 @@
 package com.example;
 
-import com.example.app.Frontend;
+import com.example.app.Argument;
+import com.example.app.ArgumentResolver;
+import com.example.app.Calculator;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 
 @EnableAutoConfiguration
 @ComponentScan
-public class App {
-    public static void main(String[] args) {
+public class App implements CommandLineRunner {
 
-        try (ConfigurableApplicationContext context = SpringApplication.run(App.class, args)) {
-            Frontend frontend = context.getBean(Frontend.class);
-            frontend.run();
-        }
+    @Autowired
+    ArgumentResolver argumentResolver;
+
+    @Autowired
+    Calculator calculator;
+
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
     }
+
+    @Override
+    public void run(String... strings) throws Exception {
+        System.out.print("Enter 2 numbers like 'a b' : ");
+        Argument argument = argumentResolver.resolve(System.in);
+        int result = calculator.calc(argument.getA(), argument.getB());
+        System.out.println("result = " + result);
+    }
+
 }
